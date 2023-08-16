15f148324bad413f6555ba4ccad0d4df7fe5bfa1
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-08-16 14:55:36 -0300

    feat: admin midleware and sessions controller

--- src/controllers/sessionsController.js
+++ src/controllers/sessionsController.js
@@ -1,28 +1,26 @@
 import bcryptjs from 'bcryptjs'
 import knex from '../database/knex/index.js'
-import { AppError } from '../utils/AppError.js'
-import jwt from 'jsonwebtoken'
+import pkg from 'jsonwebtoken'
 import authConfigs from '../configs/auth.js'
+import { AppError } from '../utils/AppError.js'
+const { sign } = pkg
 
 export class SessionsController {
   async create(request, response) {
-    const { sign } = jwt
     const { password, email } = request.body
     const user = await knex('users').where({ email }).first()
 
     if (!user) {
-      throw new AppError('User not found', 401)
+      throw new AppError('usuario não encontrado', 401)
     }
 
     const passwordMatch = await bcryptjs.compare(password, user.password)
     if (!passwordMatch) {
-      throw new AppError('User not found', 401)
+      throw new AppError('usuario não encontrado', 401)
     }
 
-    const isAdmin = user.profile_id === 1
-
     const { secret, expiresIn } = authConfigs.jwt
-    const token = sign({}, secret, {
+    const token = sign({ isAdmin: String(user.is_admin) }, secret, {
       subject: String(user.id),
       expiresIn,
     })
--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,6 +1,6 @@
 export function adminMiddleware(req, res, next) {
   if (!req.user.isAdmin) {
-    res.status(401).json({ error: 'not today, my friend' })
+    res.status(403).json({ error: 'hoje não amigo' })
   }
   next()
 }
