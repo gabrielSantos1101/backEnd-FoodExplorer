d38299bfd69d9dfdb2bccb110b1e4b55573285cd
gabrielsantos1101 <gabriel.adv45@outlook.com>
2023-08-18 14:02:57 -0300

    chore: create order finished

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -4,7 +4,7 @@ import knex from '../database/knex/index.js'
 export class OrdersController {
   async create(req, res) {
     try {
-      const { dishes, amount } = req.body
+      const { dishes } = req.body
       const user_id = req.user.id
 
       const userAddress = await knex('users')
@@ -18,18 +18,34 @@ export class OrdersController {
           .json({ message: 'Endereço do usuário não encontrado' })
       }
 
+      const dishIds = dishes.map((dish) => dish.dish_id)
+
       const orderedDishes = await knex('dishes')
-        .whereIn('id', dishes)
+        .whereIn('id', dishIds)
         .select('id', 'name', 'price')
+
+      const orderedDishesWithAmount = orderedDishes.map((dish) => {
+        const orderedDish = dishes.find((item) => item.dish_id === dish.id)
+        return {
+          ...dish,
+          amount: orderedDish.amount,
+        }
+      })
 
       const order = {
         user_id,
         user_address: userAddress.address,
-        dishes: orderedDishes,
-        amount,
+        dishes: orderedDishesWithAmount,
       }
 
-      return res.status(201).json({ order })
+      const jsonOrder = JSON.stringify(order)
+      const Id = await knex('orders').insert({
+        user_id,
+        dishes_order: jsonOrder,
+        status: 'Pendente',
+      })
+
+      return res.status(201).json({ order: { id: Id, ...order } })
     } catch (error) {
       console.error('Erro ao criar pedido:', error)
       return res.status(500).json({ message: 'Erro ao criar pedido' })
@@ -37,8 +53,16 @@ export class OrdersController {
   }
 
   async update(req, res) {
-    // const { user_id, new_status, created_at } = req.body
-    // return res.status(200).json({})
+    const { user_id, new_status } = req.body
+
+    const now = await knex.raw('SELECT NOW()')
+
+    await knex('orders').where({ id: user_id }).update({
+      status: new_status,
+      updated_at: now,
+    })
+
+    return res.status(200).json({})
   }
 
   async show(req, res) {
